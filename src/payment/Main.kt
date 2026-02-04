ec6d7f0ce593ef062bb439ecd480a1653d7a0f1c
Samael509 <pavel718234@gmail.com>
2026-02-04 15:13:29 +0300

    123

--- src/payment/Main.kt
+++ src/payment/Main.kt
@@ -1,2 +1,33 @@
 package payment
 
+fun main() {
+
+    println("\n=== работа с enum ===")
+    val cardType = CardType.VISA
+    println("тип карты: $cardType")
+    println("порядковый номер: ${cardType.ordinal}")
+    println("все типы карт: ${CardType.values().joinToString()}")
+
+    val processor = PaymentProcessor()
+    val payments = listOf(
+        Payment("4_111_111_111_111_111", 1000, CardType.VISA),
+        Payment("5_111_111_111_111_111", 1000, CardType.MASTERCARD),
+        Payment("2_222_222_222_222_222", 2000, CardType.MIR),
+        Payment("1234567812345678", 500, CardType.UNKNOWN),
+        Payment("123", 3000, CardType.VISA) //  error
+    )
+    println("=== обработка ===")
+    payments.forEach {payment ->
+        println("\nплатёж ${payment.type}: ${payment.card.take(4)}..., ${payment.sum} руб")
+        val result = processor.pay(payment)
+        processor.show(result)
+    }
+
+    val payment1 = Payment("411111111111111", 1000, CardType.VISA)
+    val payment2 = payment1.copy(type = CardType.MASTERCARD, sum = 2000)
+
+    println("\n=== сравнение data class ===")
+    println("платёж 1: $payment1")
+    println("платёж 2: $payment2")
+    println("одинаковые? ${payment1 == payment2}")
+}
--- src/payment/PaymentProcessor.kt
+++ src/payment/PaymentProcessor.kt
@@ -1,2 +1,26 @@
 package payment
 
+class PaymentProcessor {
+    private val validator = PaymentValidator()
+
+    fun pay(payment: Payment): PaymentResult {
+        if (!validator.check(payment)) {
+            return PaymentResult.Error("ошибка валидации")
+        }
+
+        return when (payment.type) {
+            CardType.VISA -> PaymentResult.Success("VISA-${System.currentTimeMillis()}")
+            CardType.MASTERCARD -> PaymentResult.Proccessing
+            CardType.MIR -> PaymentResult.Success("MIR-${System.currentTimeMillis()}")
+            CardType.UNKNOWN -> PaymentResult.Error("unknown card")
+        }
+    }
+
+    fun show(result: PaymentResult) {
+        when (result) {
+            is PaymentResult.Success -> println("успех: ${result.id}")
+            is PaymentResult.Error -> println("ошибка: ${result.reason}")
+            PaymentResult.Proccessing -> println("в обработке....")
+        }
+    }
+}
